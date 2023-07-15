d0dfe7838fe9ca61d64f1099b904053345da4b18
minwoo129 <key0129mw@naver.com>
2023-07-15 14:08:38 +0900

    NavBar css module 적용1

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,12 +5,14 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav className={styles.nav}>
+    <nav>
       <Link href="/" legacyBehavior>
-        <a>Home</a>
+        <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
       </Link>
       <Link href="/about" legacyBehavior>
-        <a>About</a>
+        <a className={router.pathname === "/about" ? styles.active : ""}>
+          About
+        </a>
       </Link>
     </nav>
   );
