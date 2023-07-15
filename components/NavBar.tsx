868590890622f007bd493b8ec1f4a4cdf66ed4ca
minwoo129 <key0129mw@naver.com>
2023-07-15 14:19:35 +0900

    navbar 스타일 모듈 삭제

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,28 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/" legacyBehavior>
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a>Home</a>
       </Link>
       <Link href="/about" legacyBehavior>
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/about" ? styles.active : ""
-          }`}
-        >
-          About
-        </a>
+        <a>About</a>
       </Link>
     </nav>
   );
