ccd7a25537389b3b85952cae066ce22ef2e61400
minwoo129 <key0129mw@naver.com>
2023-07-15 16:22:27 +0900

    APP Component 적용

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/NavBar";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,7 @@
-import NavBar from "@/components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>Hello</h1>
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
     </div>
   );
 }
