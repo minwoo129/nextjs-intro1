bae72ba21094eb401d13ee5fa4ed2cddc51ce5d9
minwoo129 <key0129mw@naver.com>
2023-07-15 16:44:59 +0900

    Head Component 적용

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Potato() {
   return (
     <div>
+      <Head>
+        <title>About | Next Movies</title>
+      </Head>
       <h1>About</h1>
     </div>
   );
